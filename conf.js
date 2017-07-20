f310521626abb4af7dae434d25b36fce84b14d3f
DESKTOP-79U09O2\Felix Livshits <a@b.com>
2017-07-20 12:43:32 -0400

    making a test commit

--- conf.js
+++ conf.js
@@ -14,6 +14,7 @@ exports.config = {
             filePrefix: 'xmloutput',
             displayStacktrace: 'none'
           });
+        //commenting silly change
           jasmine.getEnv().addReporter(junitReporter);
     },
         jasmineNodeOpts: {
